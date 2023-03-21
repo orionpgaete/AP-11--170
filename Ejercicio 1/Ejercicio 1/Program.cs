1e6bd9d64fc763bf823c230f20bfd6299572e188
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-03-21 12:08:17 -0300

    Comentario final

--- Ejercicio 1/Ejercicio 1/Program.cs
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -15,22 +15,54 @@ namespace Ejercicio_1
             //esto es un scanner
             string nombre = Console.ReadLine();
             //Console.WriteLine("Su nombre es " + nombre);
+            int edad = -1;
             Console.WriteLine("Ingrese la edad :");
             string edadTx = Console.ReadLine().Trim();
+            bool esValido = Int32.TryParse(edadTx, out edad);
+            if (!esValido)
+            {
+                Console.WriteLine("Ingrese bien la edad");
+            }
+            else
+            {
+                Console.WriteLine("Su nombre es {0} y la edad es {1}", nombre, edad);
+            }
+
+            /*
+             * string num = "12";
+             * int num2;
+             * bool esValido = int.TryParse(num, out num2);
+             * 
+             * es lo mismo que:
+             * 
+             * try{
+             *      num2=int.Parse(num);
+             *      return true;
+             * }catch(Exception ex){
+             *      return false;
+             *      }
+             */
+
+
+
+
+
+
+
             // TRIM = '    32   ' => '32'
             // TRIMSTART = '    32    ' => '32    '
             // TRIMEND = '   32   ' => '   32'
 
-            try
+           /* try
             {
                 int edad2 = Convert.ToInt32(edadTx);
             }catch (Exception ex)
             {
 
-            }
+            }*/
             
 
-            Console.WriteLine("Su nombre es {0} y la edad es {1}", nombre, edad2);
+            
             Console.ReadKey();
         }
     }
