b48ed2fc37173253942ed42c9665edd1dc4e403c
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-03-24 09:47:39 -0300

    trasformando a Clase Persona

--- Ejercicio IMC/Ejercicio IMC/Program.cs
+++ Ejercicio IMC/Ejercicio IMC/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ejercicio_IMC.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,34 @@ namespace Ejercicio_IMC
     public class Program
     {
         static void Main(string[] args)
+        {
+            while (Menu());
+        }
+
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("1. Ingresar");
+            Console.WriteLine("2. Mostrar");
+            Console.WriteLine("3. Buscar");
+            Console.WriteLine("0. Salir");
+            switch (Console.ReadLine().Trim())
+            {
+                case "1": IngresarPersona();
+                    break;
+                case "2": MostrarPersona();
+                    break;
+                case "3": BuscarPersona();
+                    break;
+                case "0": continuar = false;
+                    break;
+                default: Console.WriteLine("Aprete bien una tecla gil!!!");
+                    break;
+            }
+            return continuar;
+        }
+
+        static void IngresarPersona()
         {
             string nombre;
             uint telefono;
@@ -42,6 +71,9 @@ namespace Ejercicio_IMC
                 esValido = Double.TryParse(Console.ReadLine().Trim(), out estatura);
             } while (!esValido);
 
+            Persona p = new Persona();
+            p.Nombre = nombre;
+
             Console.WriteLine("Nombre : {0} ", nombre);
             Console.WriteLine("Telefono : {0} ", telefono);
             Console.WriteLine("Peso : {0} ", peso);
@@ -50,5 +82,16 @@ namespace Ejercicio_IMC
             Console.ReadKey();
 
         }
+
+        static void MostrarPersona()
+        {
+
+        }
+
+        static void BuscarPersona()
+        {
+
+        }
+
     }
 }
