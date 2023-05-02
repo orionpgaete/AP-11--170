a41c19a9c0c7b753fe51b79f2a75f34066afcd4e
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-05-02 12:50:20 -0400

    Socket con Libreria

---
+++ EjercicioSocket_1/Ejercicio1Cliente/Program.cs
@@ -0,0 +1,69 @@
+﻿using ClienteSocketUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Cliente
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese servidor");
+            string servidor = Console.ReadLine().Trim();
+            int puerto;
+            do
+            {
+                Console.WriteLine("Ingrese puerto");
+
+            } while (!Int32.TryParse(Console.ReadLine().Trim(), out puerto));
+
+            Console.WriteLine("Conectando...");
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+
+            if (clienteSocket.Conectar())
+            {
+                GenerarComunicacion(clienteSocket);
+            } else
+            {
+                Console.WriteLine("Error de comunicacion");
+            }
+            Console.ReadKey();
+        }
+
+        static void GenerarComunicacion(ClienteSocket clienteSocket)
+        {
+            bool terminar = false;
+            while (!terminar)
+            {
+                Console.WriteLine("En que le podemos ayudar.. : ");
+                string mensaje = Console.ReadLine().Trim();
+                clienteSocket.Escribir(mensaje);
+                if (mensaje.ToLower() == "chao")
+                {
+                    terminar = true;    
+                } else
+                {
+                    mensaje = clienteSocket.Leer();
+                    if (mensaje = != null)
+                    {
+                        Console.WriteLine("Cli: {0}", mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            terminar = true;
+                        }
+                    }else
+                    {
+                        terminar = true;
+                    }
+                    
+                }
+            }
+        }
+
+    }
+
+   
+}
---
+++ EjercicioSocket_1/EjercicioSocket_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using ServidorSocketUtils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSocket_1
+{
+   
+    public class Program
+    {
+        static void GenerarComunicacion(ClienteCom clienteCom)
+        {
+            bool terminar = false;
+            while (!terminar)
+            {
+                string mensaje = clienteCom.Leer();
+                if (mensaje != null)
+                {
+                    Console.WriteLine("Cli: {0}", mensaje);
+                    if (mensaje.ToLower() == "chao")
+                    {
+                        terminar = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ingrese respuesta : ");
+                        mensaje = Console.ReadLine().Trim();
+                        clienteCom.Escribir(mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            terminar = true;
+                        }
+                    }
+                }
+                if (terminar)
+                {
+                    clienteCom.Desconectar();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando servidor en puerto {0}", puerto);
+            ServerSocket serverSocket = new ServerSocket(puerto);
+
+            if (serverSocket.Iniciar())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Esperando Clientes...");
+                    Socket socket = serverSocket.ObtenerCliente();
+                    Console.WriteLine("Cliente recibido");
+                    //comunicarse con el cliente
+                    ClienteCom clienteCom = new ClienteCom(socket);
+                    GenerarComunicacion(clienteCom);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error al comunicarse con el puerto {0}", puerto);
+            }
+        }
+    }
+}
